b43e6ca4289705bb3c84c379adc965bfe6f31422
Andrés Marentes <andres.marentes@uniminuto.edu>
2021-01-06 23:02:04 -0500

    Rest sserver - usuario

--- server/config/config.js
+++ server/config/config.js
@@ -3,3 +3,22 @@
 //==================
 
 process.env.PORT = process.env.PORT || 3000;
+
+//==================
+//Entorno
+//==================
+
+process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
+
+//==================
+//DB
+//==================
+
+let urlDB;
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe'
+} else {
+    urlDB = 'mongodb+srv://iandresm:iAndresM0102.@iandresm.xsl3b.mongodb.net/cafe';
+}
+
+process.env.URLDB = urlDB;
